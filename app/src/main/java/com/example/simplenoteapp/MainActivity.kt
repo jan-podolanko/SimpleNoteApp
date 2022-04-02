277e1154226eb8367e3734a5b9035e232da1d67c
Jan Podolanko <jan.podolanko11@gmail.com>
2022-04-02 17:41:03 +0200

    rotation

--- app/src/main/java/com/example/simplenoteapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenoteapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,10 +19,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,7 +53,6 @@ fun NoteDisplay() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Display() {
-    val scope = rememberCoroutineScope()
 
     data class Items(val id: Int, val name: String)
 
@@ -72,7 +77,10 @@ fun Display() {
             FloatingActionButton(
                 shape = CircleShape,
                 containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-                onClick = { scope.launch { } }) {
+                onClick = {
+
+                }
+            ) {
                 Icon(Icons.Filled.Add, "")
             }
         },
@@ -80,8 +88,19 @@ fun Display() {
         content = {
             LazyColumn {
                 items(50) { i ->
+                    var rotation by remember {
+                        mutableStateOf(0f)
+                    }
+                    val rotated by animateFloatAsState(
+                        targetValue = rotation,
+                        tween(
+                            durationMillis = 3000
+                        )
+                    )
                     Surface(
-                        onClick = {},
+                        onClick = {
+                            rotation += 360f
+                        },
                         shape = RoundedCornerShape(30.dp),
                         border = BorderStroke(3.dp, MaterialTheme.colorScheme.primary),
                         color = MaterialTheme.colorScheme.primaryContainer,
@@ -89,6 +108,7 @@ fun Display() {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(horizontal = 25.dp, vertical = 5.dp)
+                            .graphicsLayer(rotationY = rotated, cameraDistance = 15f)
                     ) {
                         Text(
                             text = "Sample item ${i + 1}",
